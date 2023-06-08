f80a580bc2f25e2fffacc2eb76540b83c8f8b51a
Ismoil <ismoillmirabbosoff@gmail.com>
2023-06-09 04:11:40 +0500

    ad

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Ismoil Mirabbosoff</span> I started my professional journey with Zeromax as an automation intern and since then, I have fallen in love with their corporate culture. After working for more than 1.5 years in software industry, following are some of the highlights of my career:👇👇👇"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
